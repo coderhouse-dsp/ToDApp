2390d7337e3295398a7c7ee865e4023a89c9f757
Patel <patelde@tbdir.net>
2025-02-20 11:27:07 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# ToDo App
+
+A simple ToDo application using Flask and Azure SQL Database.
+
+## Setup
+
+1. Install the required packages:
+   ```bash
+   pip install -r requirements.txt
---
+++ app.py
@@ -0,0 +1,57 @@
+<<<<<<< HEAD
+import os
+import pyodbc
+from flask import Flask, request, render_template, redirect, url_for
+
+app = Flask(__name__)
+
+# Fetch connection string from Azure environment variables
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+# Connect to Azure SQL Database
+conn = pyodbc.connect(DATABASE_URL)
+
+@app.route('/')
+def index():
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM tasks")
+    tasks = cursor.fetchall()
+    return render_template('index.html', tasks=tasks)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+=======
+from flask import Flask, request, jsonify, render_template
+from flask_sqlalchemy import SQLAlchemy
+import urllib
+
+app = Flask(__name__)
+
+# Azure SQL Database connection string
+connection_string = "Driver={ODBC Driver 18 for SQL Server};Server=tcp:patelde-server.database.windows.net,1433;Database=todo_db;Uid=patelde-server-admin;Pwd=password;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;"
+params = urllib.parse.quote_plus(connection_string)
+app.config['SQLALCHEMY_DATABASE_URI'] = f"mssql+pyodbc:///?odbc_connect={params}"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    task = db.Column(db.String(200), nullable=False)
+
+@app.route('/')
+def index():
+    todos = Todo.query.all()
+    return render_template('index.html', todos=todos)
+
+@app.route('/add', methods=['POST'])
+def add_todo():
+    task = request.form['task']
+    new_todo = Todo(task=task)
+    db.session.add(new_todo)
+    db.session.commit()
+    return jsonify({'id': new_todo.id, 'task': new_todo.task})
+
+if __name__ == '__main__':
+    app.run(debug=True)
+>>>>>>> 1551782 (first commit)
